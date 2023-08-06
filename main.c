1c12e8d9f47e1422fd5f38aa71f2ec072cd656e9
Manuel Stoeckl <code@mstoeckl.com>
2023-08-06 13:13:07 -0400

    Fix Wayland object leaks when outputs are destroyed

--- main.c
+++ main.c
@@ -99,12 +99,18 @@ static void destroy_surface(struct swaylock_surface *surface) {
 	if (surface->layer_surface != NULL) {
 		zwlr_layer_surface_v1_destroy(surface->layer_surface);
 	}
+	if (surface->subsurface) {
+		wl_subsurface_destroy(surface->subsurface);
+	}
+	if (surface->child) {
+		wl_surface_destroy(surface->child);
+	}
 	if (surface->surface != NULL) {
 		wl_surface_destroy(surface->surface);
 	}
 	destroy_buffer(&surface->indicator_buffers[0]);
 	destroy_buffer(&surface->indicator_buffers[1]);
-	wl_output_destroy(surface->output);
+	wl_output_release(surface->output);
 	free(surface);
 }
 
